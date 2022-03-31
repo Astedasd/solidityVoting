c339f755e6c5710817903ec5bb6d50e4a1bc7554
Astedasd <asted_asd@mail.ru>
2022-04-01 00:36:24 +0700

    Test changes 2

--- test/VoteContract.test.js
+++ test/VoteContract.test.js
@@ -32,31 +32,34 @@ describe("Voting Contract", function() {
 
     describe("Deployment", function () {
 
+        // Контракт должен быть размещен
         it("should be deployed", async function () {
             expect(voting.address).to.be.properAddress;
         })
 
+        // owner должен быть правильно назначен
         it("should set the right owner", async function () {
             expect(await voting.owner()).to.equal(owner.address);
         })
+    })
 
-        
-
-        })
 
     describe("Creating Voting", function (){
 
+        // Owner может создавать голосования
         it("should be possible to create voting by owner", async function () {
             const tx = await voting.connect(owner).addVoting([addr1.address, addr2.address]);
             await tx.wait();
         })
-            
+
+        // Не owner не может создавать голосования
         it("should be impossible to create voting by non owner", async function(){
             await expect(
                 voting.connect(addr1).addVoting([addr1.address, addr2.address])
               ).to.be.revertedWith("You are not owner");
         })
 
+        // Возможно создать одновременно несколько голосований
         it("should be possible to create multiple votings", async function () {
             const tx1 = await voting.connect(owner).addVoting([addr1.address, addr2.address]);
             const tx2 = await voting.connect(owner).addVoting([addr3.address, addr4.address]);
@@ -73,34 +76,53 @@ describe("Voting Contract", function() {
         // })
         
 
-    }) 
-
-    describe("Withdraw Comission", function () {
-        //Невозможно вывести ноль
-        it("should be impossible to withdraw if there is nothing to withdraw", async function () {
-            const tx = await voting.connect(owner).addVoting([addr1.address, addr2.address]);
-            await tx.wait();
-        })
-        // возможен вывод на owner
-        // невозможен вывод на не owner
     })
 
+
+    describe("Vote for candidate", function () {
+
+    })
+
+
+    describe("Finish Voting", function () {
+        // it("should be impossible to finish voting before time ends", async function () {
+        //     const tx = await voting.connect(owner).addVoting([addr1.address, addr2.address]);
+        //     await expect(
+        //         voting.finishVoting("0")
+        //       ).to.be.revertedWith("Time not passed!");
+        // })
+    })
+
+
+    describe("Withdraw Comission", function () {
+
+        // Невозможно вывести ноль
+        it("should be impossible to withdraw if there is nothing to withdraw", async function () {
+            await expect(
+                voting.connect(owner).withdrawComission()
+              ).to.be.revertedWith("Nothing to withdraw");
+        })
         
-    
+        // Невозможен вывод на не owner
+        it("should be impossible to withdraw for not owner", async function () {
+            await expect(
+                voting.connect(addr1).withdrawComission()
+              ).to.be.revertedWith("You are not owner");
+        })
+
+        // возможен вывод на owner
+        it("should be possible to withdraw for owner", async function () {
+            // создаем голосование
+            const tx = await voting.connect(owner).addVoting([addr1.address, addr2.address]);
+            // голосуем
+
+            // завершаем голосование
+
+            // выводим комиссию
+            // await expect(
+            //     voting.connect(owner).withdrawComission()
+            //   ).to.be.revertedWith("Nothing to withdraw");
+            
+        })
+    })    
 })
-    
-
-    // it("should be possible to vote", async function(){
-    //     const tx = await voting.addVoting(acc2,acc3)
-    //     await tx.wait()
-    //   })
-
-    // it("should be possible to finish voting ", async function(){
-    //     const tx = await voting.addVoting(acc2,acc3)
-    //     await tx.wait()
-    //   })
-
-    // it("should be possible to get voting info", async function(){
-    //     const tx = await voting.addVoting(acc2,acc3)
-    //     await tx.wait()
-    //   })
